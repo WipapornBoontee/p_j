2cd7ec91fda33906fd84d3887cb87bfa8423b8c1
WipapornBoontee <148832773+WipapornBoontee@users.noreply.github.com>
2026-08-18 22:22:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include 'condb.php';
+// include 'condb.php';
 
 ?>
 
@@ -111,7 +111,7 @@ include 'condb.php';
 <body>
 
     <div id="loginFormContainer" class="form-container">
-        <h1>Login</h1>
+        <h1>Login999999999999</h1>
 
         <form action="checkLogin.php" method="post">
             <div class="form-floating mb-3">
